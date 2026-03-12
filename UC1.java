5653a8fb696f2974c85688ca0d95ec1eb0856d7d
aniruddhadeypoddar1310 <aniruddhadeypoddar1310@gmail.com>
2026-03-12 11:12:26 +0530

    UC1 added

--- UC1.java
+++ UC1.java
@@ -1,9 +1,16 @@
+// This is UC 1 
+
+/*
+@author: Sushil Kumar
+@version: 1.1
+*/
 
 public class UC1
 {
 
     public static void main(String[] args) {
         System.out.println("OOPS");
+        System.out.println("UC1 Added");
     }
 
 }
